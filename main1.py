d68e4d35bfe6e4c6b30cef4a2236f00e18478469
Manish3710 <manishwakade10@gmail.com>
2024-12-19 16:20:28 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -120,7 +120,7 @@ elif app_mode == "Prediction":
         img_array = np.array(camera_image)
         img_array_bgr = img_array[:, :, ::-1]
         #imageio.imwrite('temp.jpg', imageio.imwrite(img_array, imageio.COLOR_RGB2BGR))
-        imageio.imwrite('temp.jpg', imageio.imwrite('temp.jpg', img_array_bgr))
+        imageio.imwrite('temp.jpg', img_array_bgr))
         #imageio.imwrite('temp.jpg', imageio.imwrite('temp.jpg', img_array))
         test_image = 'temp.jpg'
     else:
